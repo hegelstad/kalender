a25fd41c1bb5c458e436e55ecc280cd747cd6d66
Nikolai Hegelstad <nikolai@hegelstad.net>
2015-02-25 13:27:00 +0100

    logincontroller update

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,13 +1,34 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class LoginController {
-    @FXML
-    private TextField username;
+    @FXML private TextField username;
+    @FXML private PasswordField password;
+    @FXML private Button loginButton;
+    @FXML private Text status;
+    boolean pressed = false;
+
+    public LoginController() {
+
+    }
 
     @FXML
-    private PasswordField password;
+    private void initialize() {
+        loginButton.setOnAction((event) -> {
+            if (!pressed) {
+                status.setText("logger inn");
+                pressed = true;
+
+            } else {
+                status.setText("logger ikke inn");
+                pressed = false;
+            }
+        });
+    }
+
 }
