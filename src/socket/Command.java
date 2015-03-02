e8fe5423ff61002c3685303c2f6722845458fecb
CPekko <eirik_fosse2@hotmail.com>
2015-03-02 14:53:01 +0100

    Serializable

--- src/socket/Command.java
+++ src/socket/Command.java
@@ -1,9 +1,11 @@
 package socket;
 
+import java.io.Serializable;
+
 /**
  * Created by sondrehj on 02.03.2015.
  */
-public class Command {
+public class Command implements Serializable{
     String command;
     
     public Command(String command){this.command = command;}
