b73192d158d2789680496708822901c28f2ccf74
tu55eladd <groneng93@gmail.com>
2015-03-03 15:14:46 +0100

    Added some testing data

--- src/controllers/CalendarEventsViewController.java
+++ src/controllers/CalendarEventsViewController.java
@@ -22,10 +22,11 @@ public class CalendarEventsViewController {
 
 	final double fullEventWidth = 158;
 	final double hourHeight = 66;
+	final double indentMargin = 15;
 	@FXML
 	GridPane weekGrid;
 	Calendar cal = new Calendar(1, "SuperKalender", null);
-	Event event = new Event(1, "Møte", null, LocalDateTime.now(), LocalDateTime.now().plusHours(1) , cal);
+	Event event = new Event(1, "Møte", null, LocalDateTime.now(), LocalDateTime.now().plusHours(2) , cal);
 	Event event2 = new Event(2, "Annet Møte", null, LocalDateTime.now().plusMinutes(15)
 			, LocalDateTime.now().plusHours(1).plusMinutes(15) , cal);
 	
@@ -43,8 +44,20 @@ public class CalendarEventsViewController {
 		for(ColumnConstraints r :weekGrid.getColumnConstraints()){
 			r.setHalignment(HPos.LEFT);
 		}
-		drawEvent(event);
-		drawEvent(event2);
+		//Lager testdata
+		ArrayList<Event> events = new ArrayList<Event>();
+		for(int i = 0;i<7;i++){
+			Event e = new Event(i, "Dag :"+i, null, LocalDateTime.of(2015, 3, 2+i, 10, 0)
+					, LocalDateTime.of(2015, 3, 2+i, 15, 0), cal);
+			events.add(e);
+		}
+		events.add(event);events.add(event2);
+		//drawEvent(event);
+		//drawEvent(event2);
+		for(Event drawSubject : events){
+			drawEvent(drawSubject);
+		}
+		
 	}
 	
 	private void drawEvent(Event event){
@@ -65,15 +78,16 @@ public class CalendarEventsViewController {
 		});
 		
 		eventRec.setOnMouseClicked(clickEvent -> {
-			Object source = clickEvent.getSource();
 			openEvent(event);				
 		});
 
+		double eventIndentMargin = indentMargin*getIndentIndex(event);
+		
 		double marginTop = ((double)event.getFrom().getMinute()/60)*hourHeight;
-		Insets eventMargin = new Insets(marginTop, 0, 0, 0);
+		Insets eventMargin = new Insets(marginTop, 0, 0, eventIndentMargin);
 		GridPane.setMargin(eventRec, eventMargin);
 		
-		Insets textMargin = new Insets(marginTop, 0, 0, 3);
+		Insets textMargin = new Insets(marginTop, 0, 0, 3 + eventIndentMargin);
 		GridPane.setMargin(eventName, textMargin);
 		
 		int dayOfWeek = event.getFrom().getDayOfWeek().getValue()-1;
@@ -91,7 +105,9 @@ public class CalendarEventsViewController {
 		LocalDateTime from = e.getFrom();
 		LocalDateTime to = e.getTo();
 		double minDiff = (to.getHour()-from.getHour())*60 + (to.getMinute()-from.getMinute());
-		return minDiff;
+		double height = minDiff/60.0 * 65.5;
+		System.out.println(height);
+		return height;
 	}
 	
 	private void styleRectangle(Rectangle r){
@@ -100,12 +116,23 @@ public class CalendarEventsViewController {
 		r.strokeProperty().set(Color.BLACK);
 		r.strokeWidthProperty().set(1);
 		r.fillProperty().set(Color.CHARTREUSE);
+		r.opacityProperty().set(0.5);
 	}
 	
 	private void styleText(Text t){
 		
 	}
 
+	private int getIndentIndex(Event event){
+//		if(event.getEventID()==1){
+//			return 0;
+//		}
+//		else{
+//			return 1;
+//		}
+		return 0;
+	}
+	
 	private void openEvent(Event event){
 		System.out.println(event.getName());
 	}
