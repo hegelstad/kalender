a5331d35af9efeca31ec6310b55484e3588fe466
CPekko <eirik_fosse2@hotmail.com>
2015-03-03 09:26:27 +0100

    More server requests

--- src/socket/Requester.java
+++ src/socket/Requester.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -21,14 +22,55 @@ import java.util.ArrayList;
 public class Requester {
 
     Socket con;
-    public Requester (Socket con){
-        this.con = con;
+    
+    public Requester (){
+    	String host = "78.91.64.114";
+
+        /** Define a port */
+        int port = 25025;
+
+        try {
+            /** Obtain an address object of the server */
+            InetAddress address = InetAddress.getByName(host);
+            /** Establish a socket connetion */
+            con = new Socket(address, port);
+            System.out.println("SocketClient initialized");
+            //connection.close();
+        }catch (IOException f) {
+            System.out.println("IOException: " + f);
+        }
+        catch (Exception g) {
+            System.out.println("Exception: " + g);
+        }
     }
 
     //USER GROUPS
     public ArrayList<UserGroup> getUserGroups(Person p){
     	Command cmd = new Command("getUserGroups-person");
-    	return null;
+    	ArrayList<UserGroup> userGroups = null;
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+            oos.writeObject(cmd);
+            oos.writeObject(userGroups);
+            InputStream is = con.getInputStream();
+            ObjectInputStream os = new ObjectInputStream(is);
+            Object o = os.readObject();
+            userGroups = (ArrayList<UserGroup>) o;
+            for (UserGroup ug : userGroups){
+                System.out.println(ug);
+            }
+
+        }  catch (ClassCastException e) {
+            System.out.println(e);
+        }
+        catch(ClassNotFoundException e){
+            System.out.println(e);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return userGroups;
     }
     
     public ArrayList<Person> getPersons(ArrayList<UserGroup> userGroups){
@@ -61,34 +103,158 @@ public class Requester {
 
     public ArrayList<UserGroup> getUserGroups(Calendar cal){
     	Command cmd = new Command("getUserGroups-calendar");
-    	return null;
+    	ArrayList<UserGroup> userGroups = null;
+    	try{
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+            oos.writeObject(cmd);
+            oos.writeObject(cal);
+            InputStream is = con.getInputStream();
+            ObjectInputStream os = new ObjectInputStream(is);
+            Object o = os.readObject();
+            userGroups = (ArrayList<UserGroup>) o;
+            for (UserGroup ug : userGroups){
+                System.out.println(ug);
+            }
+
+    	 }  catch (ClassCastException e) {
+             System.out.println(e);
+         }
+         catch(ClassNotFoundException e){
+             System.out.println(e);
+         }
+         catch (IOException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
+    	return userGroups;
     }
 
     public void deleteUserGroups(ArrayList<UserGroup> userGroups){
     	Command cmd = new Command("deleteUserGroups-userGroups");
+    	try {
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+    		oos.writeObject(cmd);
+    		oos.writeObject(userGroups);
+    	}  catch (ClassCastException e) {
+    		System.out.println(e);
+    	}
+    	catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
     }
     
     public void createUserGroups(UserGroup ug){
     	Command cmd = new Command("createUserGroup-string");
+    	try {
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+    		oos.writeObject(cmd);
+    		oos.writeObject(ug);
+    	}  catch (ClassCastException e) {
+    		System.out.println(e);
+    	}
+    	catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
     }
     
     public void addUsers(UserGroup ug){
     	Command cmd = new Command("addUsers-userGroup");
+    	try {
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+    		oos.writeObject(cmd);
+    		oos.writeObject(ug);
+    	}  catch (ClassCastException e) {
+    		System.out.println(e);
+    	}
+    	catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
     }
     
     //CALENDAR
     public ArrayList<Calendar> getCalendars(UserGroup ug){
     	Command cmd = new Command("getCalendars-usergroup");
-    	return null;
+    	ArrayList<Calendar> calendars = null;
+    	try{
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+            oos.writeObject(cmd);
+            oos.writeObject(ug);
+            InputStream is = con.getInputStream();
+            ObjectInputStream os = new ObjectInputStream(is);
+            Object o = os.readObject();
+            calendars = (ArrayList<Calendar>) o;
+            for (Calendar cal : calendars){
+                System.out.println(cal);
+            }
+
+    	 }  catch (ClassCastException e) {
+             System.out.println(e);
+         }
+         catch(ClassNotFoundException e){
+             System.out.println(e);
+         }
+         catch (IOException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
+    	return calendars;
     }
 
+    /**
+     * Tar inn kalender og userGroup, oppdaterer kalender med de nye usergroupene og returnerer kalender.
+     * @param cal
+     * @param ug
+     * @return
+     */
     public Calendar addUserGroup(Calendar cal, UserGroup ug){
     	Command cmd = new Command("addUserGroup-calendar");
-    	return null;
+    	Calendar calendar = null;
+    	try{
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+            oos.writeObject(cmd);
+            oos.writeObject(cal);
+            oos.writeObject(ug);
+            InputStream is = con.getInputStream();
+            ObjectInputStream os = new ObjectInputStream(is);
+            Object o = os.readObject();
+            calendar = (Calendar) o;
+            System.out.println(calendar);
+
+    	 }  catch (ClassCastException e) {
+             System.out.println(e);
+         }
+         catch(ClassNotFoundException e){
+             System.out.println(e);
+         }
+         catch (IOException e) {
+             // TODO Auto-generated catch block
+             e.printStackTrace();
+         }
+    	return calendar;
     }
     
+    /**
+     * Tar inn kalender og UserGroup og fjerner usergroup fra kalender
+     * @param cal
+     * @param ug
+     */
     public void removeUserGroup(Calendar cal, UserGroup ug){
     	Command cmd = new Command("removeUserGroup-calendar");
+     	try {
+    		ObjectOutputStream oos = new ObjectOutputStream(con.getOutputStream());
+    		oos.writeObject(cmd);
+    		oos.writeObject(cal);
+    		oos.writeObject(ug);
+    	}  catch (ClassCastException e) {
+    		System.out.println(e);
+    	}
+    	catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
     }
     
     public Calendar createCalendar(Calendar cal){
@@ -188,5 +354,6 @@ public class Requester {
     	Command cmd = new Command("getAvailableRooms-event");
     	return null;
     }
+
  
 }
