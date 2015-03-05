c5c5a0582a84ff16332bbbc2d3e8981c3950f60f
CPekko <eirik_fosse2@hotmail.com>
2015-03-05 12:47:17 +0100

    Requester for attendants and new model Attendant.

---
+++ src/models/Attendant.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.io.Serializable;
+
+public class Attendant implements Serializable{
+    private int userGroupID;
+    private String name;
+    private int status;
+
+    public Attendant(int userGroupID, String name, int status) {
+        this.userGroupID = userGroupID;
+        this.name = name;
+        this.status = status;
+    }
+    
+    public int getUserGroupID() {
+		return userGroupID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setUserGroupID(int userGroupID) {
+		this.userGroupID = userGroupID;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	@Override
+    public String toString(){
+        return name;
+    }
+}
