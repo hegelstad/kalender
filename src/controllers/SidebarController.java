9e6e0b168f0dbbbc1856732ee6085730e14f0995
tu55eladd <groneng93@gmail.com>
2015-03-06 15:26:22 +0100

    Started on method for add calendars with checkboxes on sidebar

--- src/controllers/SidebarController.java
+++ src/controllers/SidebarController.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import models.Calendar;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 
 public class SidebarController {
 
@@ -11,6 +15,26 @@ public class SidebarController {
 	//Is supposed to set the defaultCalendar text to the currently logged in user's username.
 	@FXML private void initialize(){
 		//defaultCalendar.setText("Test");
+	}
+	
+	private void addCalendar(Calendar cal){
+		
+		CheckBox checkbox = new CheckBox();
+		Label label = new Label(cal.getName());
+		
+		checkbox.selectedProperty().addListener( (ob,oldVal,newVal) -> {
+			if(newVal)
+			{
+				WeekController.getController().addCalendarEvents(cal);
+			}
+			else
+			{
+				WeekController.getController().removeCalendarEvents(cal);
+			}
+			}
+		);
+		
+		/* Must add new button and label to scene here */
 	}
 	
 }
