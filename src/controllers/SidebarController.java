eb8a42f2e948c6428b1b605b0777339fc8c016a1
Sondre <sondrehjetland@gmail.com>
2015-03-13 14:33:43 +0100

    Funksjonalitet for å søke etter kalendere i real-time

--- src/controllers/SidebarController.java
+++ src/controllers/SidebarController.java
@@ -2,6 +2,10 @@ package controllers;
 
 import java.util.ArrayList;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
 import models.Calendar;
 import models.PersonInfo;
 import javafx.collections.FXCollections;
@@ -13,6 +17,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import models.CalendarCell;
+import socket.Requester;
+
 public class SidebarController {
 
 	private static SidebarController controller;
@@ -20,6 +26,7 @@ public class SidebarController {
 	@FXML private ListView<Calendar> calendarList;
 	@FXML private Pane userManagmentPaneButton;
 	@FXML private TextField searchCalendar;
+	@FXML private ListView<Calendar> subscribeCalendarList;
 	
 	ObservableList<Calendar> calendars = FXCollections.observableArrayList(new ArrayList<Calendar>());
 	
@@ -28,6 +35,7 @@ public class SidebarController {
 		Rectangle rectangle = new Rectangle(1,1);
 		searchCalendar.setShape(rectangle);
 		updateCalendarList();
+		initSearchCalendar();
 		for(Calendar cal : PersonInfo.getPersonInfo().getAllCalendars()){
 			System.out.println(cal);
 		}
@@ -84,6 +92,40 @@ public class SidebarController {
 	public ListView<Calendar> getListView(){
 		return calendarList;
 	}
+	
+	public void initSearchCalendar (){
+		//Henter alle kalendere og legger de til i en observable list
+		Requester r = new Requester();
+		ArrayList<Calendar> allCalendars = r.getAllCalendars();
+		r.closeConnection();
+		searchCalendar.setStyle("-fx-text-fill: #000000");
+		ObservableList<Calendar> masterData = FXCollections.observableArrayList(allCalendars);
+		r.closeConnection();
+		//Setter CellFactory
+		subscribeCalendarList.setCellFactory((list) -> {
+			return new CalendarCell();
+		});
+		//ChangeListener som lytter på endringer i tekstfeltet og finner kalendere som inneholder
+		//teksten
+		searchCalendar.textProperty().addListener((observable, oldValue, newValue) -> {
+			ArrayList<Calendar> filteredCalendars = new ArrayList<>();
+			ObservableList<Calendar> filteredData = FXCollections.observableArrayList();
+            for (Calendar cal : masterData){
+				if(cal.getName().contains(searchCalendar.getText())){
+					filteredCalendars.add(cal);
+				}
+			}
+			//Fjerner alle søkeresultater om tekstfeltet er tomt
+			if(searchCalendar.getText().length() == 0){
+				subscribeCalendarList.setItems(null);
+			} else {
+				//Setter innholder av listviewt til resultatet vi har filtrert ut
+				filteredData = FXCollections.observableArrayList(filteredCalendars);
+				subscribeCalendarList.setItems(filteredData);
+			}
+		});
+	}
+	
 	
 
 	
